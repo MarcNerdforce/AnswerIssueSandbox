98fa4189b05054fbfcc781e65eeb0e5f8d75fb28
MarcNerdforce <76945940+MarcNerdforce@users.noreply.github.com>
2021-03-29 13:34:00 +0200

    Initial commit

---
+++ src/components/SurveyComponent.vue
@@ -0,0 +1,39 @@
+<template>
+  <div v-if="!loading">
+    <h1>we are loaded</h1>
+    <survey :survey="survey" />
+  </div>
+</template>
+
+<script>
+import * as Survey from "survey-vue";
+import "survey-vue/modern.css";
+import "./index.css";
+Survey.settings.lazyRowsRendering = true;
+Survey.StylesManager.applyTheme("modern");
+
+const json = require("../components/survey.json");
+const answers_test = require("../components/answers_test.json");
+
+export default {
+  name: "surveyjs-component",
+  data(cmp) {
+    let loading = true;
+    let survey = {};
+    let doStuff = async () => {};
+    doStuff().then((r) => {
+      cmp.survey = new Survey.Model(json);
+      cmp.loading = false;
+      cmp.survey.data = answers_test;
+    });
+    return {
+      loading,
+      survey,
+    };
+  },
+  mounted() {},
+};
+</script>
+
+<style scoped>
+</style>
